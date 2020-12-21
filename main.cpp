1a03a4c9a56bca4f52a9c7e77fdce34b16bad9e9
matveybaykalov <70635348+matveybaykalov@users.noreply.github.com>
2020-12-21 15:23:03 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,121 +1,120 @@
 #include <iostream>
 #include <string>
 #include <ctime>
-#include <vector>
 #include <fstream>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::endl;
-using std::vector;
 using std::ifstream;
 using std::ofstream;
-
-string GenerateGamma(){
-    string gamma;
-    for(int i = 0; i<16; ++i){
-        gamma += static_cast<char>(std::rand()%64+192);
-    }
-    return gamma;
-}
-
-string XORencoder (string text, const vector<string>& gamma){
-    int CharIter = 0;
-    int VectorIter = 0;
-    for (auto& i: text){
-        i ^= gamma[VectorIter][CharIter];
-        ++CharIter;
-        if (CharIter%16 == 0){
-            ++VectorIter;
-            CharIter %= 16;
-        }
-    }
-    return text;
-}
 
 struct Blok{
     unsigned long long first = 0;
     unsigned long long second = 0 ;
 };
 
-string LeftShift (const string& text){
-    string chifrotext;
-    for (int i = 0; i < (text.length()/16)+1; ++i){
-        Blok blok;
-        for(int j = 0; j < 8; ++j){
-            if (i*16+j<text.length()) blok.first = blok.first*256+static_cast<unsigned char>(text[i*16+j]);
-            if (i*16+8+j<text.length()) blok.second = blok.second*256+static_cast<unsigned char>(text[i*16+8+j]);
-        }
-        unsigned char temp = blok.first >> 58;
-        unsigned char temp2 = blok.second >> 58;
-        blok.first = blok.first << 6;
-        blok.second = blok.second << 6;
-        blok.first += temp2;
-        blok.second += temp;
-        string SubStr;
-        for (int j = 0; j < 8; ++j){
-            string s1;
-            s1 += static_cast<char>(blok.first%256);
-            SubStr.insert(0, s1);
-            blok.first /= 256;
-            string s2;
-            s2 += static_cast<char>(blok.second%256);
-            SubStr.insert(j+1, s2);
-            blok.second /= 256;
-        }
-        chifrotext +=SubStr;
+Blok GenerateGamma(){
+    Blok gamma;
+    for(int i = 0; i<8; ++i){
+        gamma.first <<= 8;
+        gamma.first += rand() % 256;
+        gamma.second <<= 8;
+        gamma.second += rand() % 256;
     }
-    return chifrotext;
+    return gamma;
 }
 
-string RightShift (const string& text) {
-    string chifrotext;
-    for (int i = 0; i < text.length() / 16; ++i) {
-        Blok blok;
-        for (int j = 0; j < 8; ++j) {
-            blok.first = blok.first * 256 + static_cast<unsigned char>(text[i * 16 + j]);
-            blok.second = blok.second * 256 + static_cast<unsigned char>(text[i * 16 + 8 +j]);
-        }
-        unsigned long long temp = blok.first << 58;
-        unsigned long long temp2 = blok.second << 58;
-        blok.first = blok.first >> 6;
-        blok.second = blok.second >> 6;
-        blok.first += temp2;
-        blok.second += temp;
-        string SubStr;
-        for (int j = 0; j < 8; ++j) {
-            if (blok.first % 256 != 0){
-                string s1;
-                s1 += static_cast<char>(blok.first % 256);
-                SubStr.insert(0, s1);
-            }
-            blok.first /= 256;
-            if (blok.second % 256 != 0){
-                string s2;
-                s2 += static_cast<char>(blok.second % 256);
-                SubStr.insert(j + 1, s2);
-            }
-            blok.second /= 256;
-        }
-        chifrotext += SubStr;
+void operator<< (unsigned long long& number, const string& str){
+    for (unsigned char ch : str){
+        number <<= 8;
+        number += ch;
     }
-    return chifrotext;
+}
+
+void operator<< (string& str, unsigned long long& number){
+    for (int i = 0; i < 8; ++i){
+        string s1;
+        s1 = static_cast<char>(number % 256); //пришлось создать строку из 1 символа, чтобы воспользоваться функцией insert
+        str.insert(0, s1);
+        number >>= 8;
+    }
+}
+
+void operator<< (Blok& blok, const int& n){
+    int a = 64-n;
+    unsigned char temp = blok.first >> a;
+    unsigned char temp2 = blok.second >> a;
+    blok.first = blok.first << n;
+    blok.second = blok.second << n;
+    blok.first += temp2;
+    blok.second += temp;
+}
+
+void operator>> (Blok& blok, const int& n){
+    int a = 64-n;
+    unsigned long long temp = blok.first << a;
+    unsigned long long temp2 = blok.second << a;
+    blok.first = blok.first >> 6;
+    blok.second = blok.second >> 6;
+    blok.first += temp2;
+    blok.second += temp;
+}
+
+void operator^(Blok& blok, const Blok& gamma){
+    blok.first ^= gamma.first;
+    blok.second ^= gamma.second;
+}
+
+void encoding (string& text, string& AddressOutput){
+    ofstream output (AddressOutput, std::ios::binary);
+    //эта функция сразу будет проводить побитовое ИЛИ с 16-байтовыми числами и побитовый сдвиг
+    while (text.length()%16 != 0){
+        text += '\n';
+    }
+    for (int i = 0; i < text.length()/16; ++i){
+        Blok blok;
+        blok.first << text.substr(i*16, 8);
+        blok.second << text.substr(i*16+8, 8);
+        blok ^ GenerateGamma();
+        blok << 6;
+        output.write(reinterpret_cast<const char *>(&blok.first), sizeof(blok.first));
+        output.write(reinterpret_cast<const char *>(&blok.second), sizeof(blok.second));
+    }
+}
+
+string decoding (const string& AddressInput) {
+    ifstream input (AddressInput, std::ios::binary);
+    Blok blok;
+    string text;
+    while (input.read(reinterpret_cast<char *>(&blok.first), sizeof(blok.first))){
+        input.read(reinterpret_cast<char *>(&blok.second), sizeof(blok.second));
+        blok >> 6;
+        blok ^ GenerateGamma();
+        string s1, s2;
+        s1 << blok.first;
+        s2 << blok.second;
+        text += s1 + s2;
+    }
+    while (text[text.length()-1] == '\n'){
+        text.erase(text.length()-1, 1);
+    }
+    return text;
 }
 int main (){
-    string AdressInput, AdressOutput;
+    string AddressInput, AddressOutput;
     int operation;
     cout << "Select the program mode" << endl;
     cout << "1. encoding" << endl << "2. decoding" << endl;
     cin >> operation;
     if (operation == 1) {
-        vector <string> gammas;
         string text, str;
         cout << "Specify the full path to the file input.txt" << endl;
-        cin >> AdressInput;
+        cin >> AddressInput;
         cout << "Specify the full path to the file output.txt" << endl;
-        cin >> AdressOutput;
-        ifstream input(AdressInput);
+        cin >> AddressOutput;
+        ifstream input(AddressInput);
         while (std::getline(input, str)) {
             text += str + '\n';
         }
@@ -123,34 +122,19 @@ int main (){
         int key = std::rand();
         std::srand(key);
         cout << "Remember this key: " << key;
-        for (int i = 0; i < ceil(text.length() / 16.); ++i) {
-            gammas.push_back(GenerateGamma());
-        }
-        string XORtext = XORencoder(text, gammas);
-        string chifrotext = LeftShift(XORtext);
-        ofstream output(AdressOutput);
-        output << chifrotext;
+        encoding(text, AddressOutput);
     } else {
-        string text, chifrotext, str, gamma;
-        vector <string> gammas;
+        string text;
         cout << "Specify the full path to the file input.txt" << endl;
-        cin >> AdressInput;
+        cin >> AddressInput;
         cout << "Specify the full path to the file output.txt" << endl;
-        cin >> AdressOutput;
-        ifstream input(AdressInput);
-        while (std::getline (input, str)){
-            chifrotext += str;
-        }
+        cin >> AddressOutput;
         cout << "Enter the key" << endl;
         int key;
         cin >> key;
         std::srand(key);
-        for (int i = 0; i < chifrotext.length()/16; ++i){
-            gammas.push_back(GenerateGamma());
-        }
-        string unchifrotext = RightShift(chifrotext);
-        text = XORencoder(unchifrotext, gammas);
-        ofstream output(AdressOutput);
+        text = decoding(AddressInput);
+        ofstream output (AddressOutput);
         output << text;
     }
     return 0;
